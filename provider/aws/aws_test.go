62cc0a12c128f366856e56946612aa0bd5549a8b
Alfred Krohmer <devkid@devkid.net>
2020-11-10 15:11:24 +0100

    Fix tests

--- provider/aws/aws_test.go
+++ provider/aws/aws_test.go
@@ -743,8 +743,6 @@ func TestAWSsubmitChangesRetryOnError(t *testing.T) {
 	ctx := context.Background()
 	zones, err := provider.Zones(ctx)
 	require.NoError(t, err)
-	records, err := provider.Records(ctx)
-	require.NoError(t, err)
 
 	ep1 := endpoint.NewEndpointWithTTL("success.zone-1.ext-dns-test-2.teapot.zalan.do", endpoint.RecordTypeA, endpoint.TTL(recordTTL), "1.0.0.1")
 	ep2 := endpoint.NewEndpointWithTTL("fail.zone-1.ext-dns-test-2.teapot.zalan.do", endpoint.RecordTypeA, endpoint.TTL(recordTTL), "1.0.0.2")
@@ -756,7 +754,7 @@ func TestAWSsubmitChangesRetryOnError(t *testing.T) {
 	}
 
 	// "success" and "fail" are created in the first step, both are submitted in the same batch; this should fail
-	cs1 := provider.newChanges(route53.ChangeActionCreate, []*endpoint.Endpoint{ep2, ep2txt, ep1}, records, zones)
+	cs1 := provider.newChanges(route53.ChangeActionCreate, []*endpoint.Endpoint{ep2, ep2txt, ep1}, zones)
 	input1 := &route53.ChangeResourceRecordSetsInput{
 		HostedZoneId: aws.String("/hostedzone/zone-1.ext-dns-test-2.teapot.zalan.do."),
 		ChangeBatch: &route53.ChangeBatch{
@@ -766,7 +764,7 @@ func TestAWSsubmitChangesRetryOnError(t *testing.T) {
 	clientStub.MockMethod("ChangeResourceRecordSets", input1).Return(nil, fmt.Errorf("Mock route53 failure"))
 
 	// because of the failure, changes will be retried one by one; make "fail" submitted in its own batch fail as well
-	cs2 := provider.newChanges(route53.ChangeActionCreate, []*endpoint.Endpoint{ep2, ep2txt}, records, zones)
+	cs2 := provider.newChanges(route53.ChangeActionCreate, []*endpoint.Endpoint{ep2, ep2txt}, zones)
 	input2 := &route53.ChangeResourceRecordSetsInput{
 		HostedZoneId: aws.String("/hostedzone/zone-1.ext-dns-test-2.teapot.zalan.do."),
 		ChangeBatch: &route53.ChangeBatch{
@@ -779,7 +777,7 @@ func TestAWSsubmitChangesRetryOnError(t *testing.T) {
 	require.Error(t, provider.submitChanges(ctx, cs1, zones))
 
 	// assert that "success" was successfully created and "fail" and its TXT record were not
-	records, err = provider.Records(ctx)
+    records, err := provider.Records(ctx)
 	require.NoError(t, err)
 	ok := false
 	for _, r := range records {
@@ -793,7 +791,7 @@ func TestAWSsubmitChangesRetryOnError(t *testing.T) {
 	require.True(t, ok)
 
 	// next batch should contain "fail" and "success2", should succeed this time
-	cs3 := provider.newChanges(route53.ChangeActionCreate, []*endpoint.Endpoint{ep2, ep2txt, ep3}, records, zones)
+	cs3 := provider.newChanges(route53.ChangeActionCreate, []*endpoint.Endpoint{ep2, ep2txt, ep3}, zones)
 	require.NoError(t, provider.submitChanges(ctx, cs3, zones))
 
 	// verify all records are there
